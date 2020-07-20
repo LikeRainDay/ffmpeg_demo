8aa04ade42ece7eb64b19e86b57b55f987a229a0
候帅 <houshuai0816@126.com>
2020-07-20 11:17:01 +0800

    提交推流测试

---
+++ test/rtmp/test_rtmp_pusher.h
@@ -0,0 +1,13 @@
+//
+// Created by 候帅 on 2020/6/5.
+//
+
+#ifndef HC_DEMO_TEST_RTMP_PUSHER_H
+#define HC_DEMO_TEST_RTMP_PUSHER_H
+
+class test_rtmp_pusher {
+public:
+    int doPush();
+};
+
+#endif //HC_DEMO_TEST_RTMP_PUSHER_H
